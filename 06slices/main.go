314c2b5b88af46c1b4caab04cc6f06c1922157fb
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-10-09 01:22:25 +0530

    Add high scores demonstration with sorting in main.go

--- 06slices/main.go
+++ 06slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 
@@ -12,5 +15,19 @@ func main() {
 
 	fruit = append(fruit[1:])
 	fmt.Println("Fruit List after removing Apple: ", fruit)
+
+	highScores := make([]int, 4)
+	highScores[0] = 234
+	highScores[1] = 456
+	highScores[2] = 678
+	highScores[3] = 890
+	fmt.Println("High Scores: ", highScores)
+	
+	highScores = append(highScores, 555, 666, 777)
+	fmt.Println("High Scores after appending new scores: ", highScores)
+	sort.Ints(highScores)
+	fmt.Println("High Scores after sorting: ", highScores)
+
+	
 
 }
