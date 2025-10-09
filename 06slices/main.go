468d767770ea29157e7d13e844213c8eea9899ac
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-10-10 03:03:16 +0530

    more with slices

--- 06slices/main.go
+++ 06slices/main.go
@@ -10,10 +10,10 @@ func main() {
 	var fruit = []string{}
 	fmt.Println("Type of fruitList is   ", fmt.Sprintf("%T", fruit))
 
-	fruit = append(fruit, "Apple" , "Banana", "Grapes")
+	fruit = append(fruit, "Apple", "Banana", "Grapes")
 	fmt.Println("Fruit List after adding Apple: ", fruit)
 
-	fruit = append(fruit[1:])
+	fruit = append(fruit[1:], "Kiwi")
 	fmt.Println("Fruit List after removing Apple: ", fruit)
 
 	highScores := make([]int, 4)
@@ -22,12 +22,10 @@ func main() {
 	highScores[2] = 678
 	highScores[3] = 890
 	fmt.Println("High Scores: ", highScores)
-	
+
 	highScores = append(highScores, 555, 666, 777)
 	fmt.Println("High Scores after appending new scores: ", highScores)
 	sort.Ints(highScores)
 	fmt.Println("High Scores after sorting: ", highScores)
-
-	
 
 }
